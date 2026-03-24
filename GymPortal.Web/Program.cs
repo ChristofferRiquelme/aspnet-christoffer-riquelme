7d0f0f6b2eb20587edcec9b3f648ac6d4c0c76ff
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2026-03-24 17:22:42 +0100

    Add builder.Services to Program

--- GymPortal.Web/Program.cs
+++ GymPortal.Web/Program.cs
@@ -1,7 +1,15 @@
+using GymPortal.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=gym.db"));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -25,5 +33,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
+app.MapRazorPages();
 app.Run();
