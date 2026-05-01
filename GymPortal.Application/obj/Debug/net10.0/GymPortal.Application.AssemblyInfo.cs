bddffc9a95c2a36915a7fe7626a7633b20c25eab
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2026-05-01 21:39:56 +0200

    Add Our Program section in Home

--- GymPortal.Application/obj/Debug/net10.0/GymPortal.Application.AssemblyInfo.cs
+++ GymPortal.Application/obj/Debug/net10.0/GymPortal.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymPortal.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2fc9564e9b2b63862098c36d33278c5b471ef7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d959463a339562b197beab9c3c04f816abb5ecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymPortal.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymPortal.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
